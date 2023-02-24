26f9111bdb33f1d7b4766272dc20911c734ae029
Akaash Banerjee <akaash@Akaashs-Samsung-Smart-Fridge.local>
2023-02-24 14:05:43 +0800

    idek

---
+++ getGnomed.java
@@ -0,0 +1,15 @@
+
+//dont even ask idk
+public class getGnomed {
+	
+	public static void main (String[] args) {
+		
+		
+		for(int i = 1; i>0; i++){
+			
+			System.out.println("me more >:D");
+			
+			}
+	}
+}
+
