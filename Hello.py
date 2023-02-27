a5521087433e2bbf09b83de17cf27c80551f5630
Akaash Banerjee <akaash@Akaashs-Samsung-Smart-Fridge.local>
2023-02-27 19:34:34 +0800

    idek what this is

---
+++ Hello.py
@@ -0,0 +1,12 @@
+print("What if we opened a Halloween store, only, in January?")
+print('*' * 10)
+print ('*' * 9)
+print('*' * 8)
+print ('*' * 7)
+print('*' * 6)
+print ('*' * 5)
+print ('*' * 4)
+print ('*' * 3)
+print ('*' * 2)
+print ('*' * 1)
+
