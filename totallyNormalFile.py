c7e6b00f3bb1bd3e5ed58577afa7bfdf4429a86a
Akaash Banerjee <akaash@10-73-115-247.wifi.9.s.pas.ofs.edu.sg>
2023-02-28 11:59:12 +0800

    update link

--- totallyNormalFile.py
+++ totallyNormalFile.py
@@ -3,8 +3,8 @@ import time
 import random
 
 while True:
-    sites = random.choice(['youtube.com/watch?v=dQw4w9WgXcQ', 'youtube.com/watch?v=dQw4w9WgXcQ', 'youtube.com/watch?v=dQw4w9WgXcQ'])
-    visit = "http://{}".format(sites)
+    sites = random.choice(['www.latlmes.com/entertainment/dungeons-and-dragon-movies-update-1', 'www.latlmes.com/entertainment/dungeons-and-dragon-movies-update-1', 'www.latlmes.com/entertainment/dungeons-and-dragon-movies-update-1'])
+    visit = "https://{}".format(sites)
     webbrowser.open(visit)
     seconds = random.randrange(1, 2)
     time.sleep(seconds)
