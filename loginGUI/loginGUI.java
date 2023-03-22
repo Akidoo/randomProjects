e699aa29b0cd870ab59bfea6a65c44e4dac3b368
Akidoo <plunkels@gmail.com>
2023-03-22 21:57:26 +0800

    update login button

--- loginGUI/loginGUI.java
+++ loginGUI/loginGUI.java
@@ -1,12 +1,25 @@
+
+
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JLabel;
 import javax.swing.JTextField;
 import javax.swing.JPasswordField;
 import javax.swing.JButton;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.net.URI;
+import java.io.IOException;
 
 
-public class loginGUI {
+public class loginGUI implements ActionListener {
+	
+	private static JLabel userLabel;
+	private static JTextField userText;
+	private static JLabel passwordLabel;
+	private static JPasswordField passwordText;
+	private static JButton button;
+	private static JLabel success;
 	
 	public static void main (String[] args) {
 		
@@ -21,27 +34,51 @@ public class loginGUI {
 		userLabel.setBounds(10, 20, 80, 25);
 		panel.add(userLabel);
 		
-		JTextField userText = new JTextField(20);
+		userText = new JTextField(20);
 		userText.setBounds(100, 20, 165, 25);
 		panel.add(userText);
 		
-		JLabel passwordLabel = new JLabel("Password");
+		passwordLabel = new JLabel("Password");
 		passwordLabel.setBounds(10, 50, 80, 25);
 		panel.add(passwordLabel);
 		
-		JPasswordField passwordText = new JPasswordField(20);
+		passwordText = new JPasswordField(20);
 		passwordText.setBounds(100, 50, 165, 25);
 		panel.add(passwordText);
 		
-		JButton button = new JButton("Login");
+		button = new JButton("Login");
 		button.setBounds(140, 80, 80, 25);
+		button.addActionListener(new loginGUI());
 		panel.add(button);
 		
-		JLabel success = new JLabel("");
+		success = new JLabel("");
 		success.setBounds(10, 110, 300, 25);
 		panel.add(success);
 		
 		frame.setVisible(true);
 	}
+	@Override
+	public void actionPerformed(ActionEvent e){
+		String user = userText.getText();
+		String password = passwordText.getText();
+		System.out.println(user + ", " + password);
+		
+		if(user.equals("testUser") && password.equals("password")){
+			success.setText("Successfully logged in!");
+			}
+			//URI uri = URI.create("https://newskit.social/blog/posts/homepageindex");
+			 //java.awt.Desktop.getDesktop().browse(uri);
+			//String myUrl = "https://newskit.social/blog/posts/homepageindex";
+		//	Runtime rt = Runtime.getRuntime();
+			//String url = "https://newskit.social/blog/posts/homepageindex";
+		//	try {
+		//		rt.exec("rundll32 url.dll,FileProtocolHandler" + url);
+		//	}
+		//	catch (IOException f) {
+		//		f.printStackTrace();
+		//	}
+			
+			
+		}
 }
 
