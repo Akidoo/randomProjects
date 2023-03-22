15f89f0fd066c271fa2d820f952384f67ea51ab6
Akidoo <plunkels@gmail.com>
2023-03-22 23:25:36 +0800

    trying to add a redirect

--- loginGUI/loginGUI.java
+++ loginGUI/loginGUI.java
@@ -1,5 +1,6 @@
-
-
+import java.net.URL;
+import java.net.MalformedURLException;
+import java.awt.Desktop;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JLabel;
@@ -9,10 +10,13 @@ import javax.swing.JButton;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.net.URI;
+import java.net.URISyntaxException;
 import java.io.IOException;
 
 
 public class loginGUI implements ActionListener {
+	
+	String[] cmd = {"bash","-c","google-chrome https://newskit.social/blog/posts/homepageindex"};
 	
 	private static JLabel userLabel;
 	private static JTextField userText;
@@ -58,25 +62,30 @@ public class loginGUI implements ActionListener {
 		frame.setVisible(true);
 	}
 	@Override
-	public void actionPerformed(ActionEvent e){
+	public void actionPerformed(ActionEvent e) {
 		String user = userText.getText();
 		String password = passwordText.getText();
 		System.out.println(user + ", " + password);
 		
 		if(user.equals("testUser") && password.equals("password")){
 			success.setText("Successfully logged in!");
+			//URL link = new URL("https://newskit.social/blog/posts/homepageindex");
+			
+			//Desktop desk = Desktop.getDesktop();
+			//desk.browse(link.toURI());
 			}
 			//URI uri = URI.create("https://newskit.social/blog/posts/homepageindex");
 			 //java.awt.Desktop.getDesktop().browse(uri);
 			//String myUrl = "https://newskit.social/blog/posts/homepageindex";
-		//	Runtime rt = Runtime.getRuntime();
+			//Runtime rt = Runtime.getRuntime();
 			//String url = "https://newskit.social/blog/posts/homepageindex";
-		//	try {
-		//		rt.exec("rundll32 url.dll,FileProtocolHandler" + url);
-		//	}
-		//	catch (IOException f) {
-		//		f.printStackTrace();
-		//	}
+			//try {
+				//rt.exec("runso url.so,FileProtocolHandler " + url);
+			//}
+			//catch (IOException f) {
+				//f.printStackTrace();
+			//}
+			
 			
 			
 		}
